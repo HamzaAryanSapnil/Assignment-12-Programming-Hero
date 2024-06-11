5b00c8e3e7a7910e67ae08ce8b2a059dec3db6d6
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-06-11 16:38:28 +0600

    secured to load users

--- src/Hooks/useLoadUsers.jsx
+++ src/Hooks/useLoadUsers.jsx
@@ -1,15 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
-import useAxiosPublic from "./useAxiosPublic";
 
 const useLoadUsers = () => {
-    // const axiosSecure = useAxiosSecure();
-    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
       queryFn: async () => {
         // i have to make this to axios secure
-      const res = await axiosPublic.get("/users");
+      const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
