52eda0430b6e0b1294ac1161181dbcecd57d34eb
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-08-31 17:34:33 +0600

    deployed in firebase

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "http://localhost:3000",
+  baseURL: "https://assignment-12-tourist-guide-server-side.vercel.app",
+  // baseURL: "https://assignment-12-tourist-guide-server-side.vercel.app",
 });
 
 const useAxiosPublic = () => {
